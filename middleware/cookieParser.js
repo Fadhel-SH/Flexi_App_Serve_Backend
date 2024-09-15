7925341c01a9890de14ef24b93db9acfcd98a867
Hussain Alsabba <hsabba97@gmail.com>
2024-09-16 02:51:34 +0300

    Remove node_modules and .env from tracking

--- middleware/cookieParser.js
+++
@@ -1,8 +0,0 @@
-app.use(cookieParser());
-
-// new code below
-app.use(session({
-  secret: process.env.SECRET,
-  resave: false,
-  saveUninitialized: true
-}));
--- middleware/express-session.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const session = require('express-session');
-
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false } 
-}));
